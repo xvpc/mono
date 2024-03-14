63befab343f2e9d7594f4bd97df02d68ec6dbcae
Viper <viperxx100@gmail.com>
2024-03-14 15:09:39 +0300

    2024-03-14

--- main.sh
+++ main.sh
@@ -4,7 +4,10 @@ REPO_LINK="https://github.com/xvpc/mono.git"
 
 count=1
 current_date=$(date +"%Y-%m-%d")
-# 432 commit | 2024-03-12
+
+red='\033[0;31m'
+green='\033[0;32m'
+default='\033[0m'
 
 echo "Started script"
 
@@ -18,11 +21,11 @@ while [[ $count -le 200 ]]; do
     echo "$formatted_date" >> commit.txt 
     git add *
     git commit -m "${current_date}"
-    echo "$formatted_date"
+    echo -e "${green}$formatted_date${default}"
     count=$((count + 1))
 done
 
-git branch -M main
-git push -u origin main -f
+# git branch -M main
+# git push -u origin main -f
 
 echo "Completed script"
